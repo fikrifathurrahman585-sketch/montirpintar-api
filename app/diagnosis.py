cbd4f5ce77c878c24f29124a2dc80ddb9326d1b2
fikrifathurrahman585-sketch <fikrifathurrahman585@gmail.com>
2026-08-01 22:15:23 +0700

    Update diagnosis.py

--- app/diagnosis.py
+++ app/diagnosis.py
@@ -3,6 +3,8 @@ import os
 from app.language import normalize_slang, detect_language
 from app.search import semantic_search
 from app.formatter import format_response
+from app.loader import load_cars
+from app.loader import load_motorcycles
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -14,8 +16,8 @@ def load_json_db(filename):
         return []
 
 # Muat database mobil dan motor
-DATABASE_CARS = load_json_db("cars.json")
-DATABASE_MOTORCYCLES = load_json_db("motorcycles.json")
+DATABASE_CARS = load_cars()
+DATABASE_MOTORCYCLES = load_motorcycles()
 FULL_DATABASE = DATABASE_CARS + DATABASE_MOTORCYCLES
 
 def analyze_symptom(user_input: str):
