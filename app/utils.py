4f9be7df4d32d522231fe76ec31a9b1e9b8aeea4
fikrifathurrahman585-sketch <fikrifathurrahman585@gmail.com>
2026-08-01 17:33:14 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,16 @@
+from collections import Counter
 
+
+def duplicate(items):
+
+    counter = Counter(items)
+
+    return [
+
+        item
+
+        for item, count in counter.items()
+
+        if count > 1
+
+    ]
