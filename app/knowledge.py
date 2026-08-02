52ecc332edec4ff49d513c816353274f0d3402b8
fikrifathurrahman585-sketch <fikrifathurrahman585@gmail.com>
2026-08-03 00:29:58 +0700

    Create knowledge.py

---
+++ app/knowledge.py
@@ -0,0 +1,63 @@
+# ==========================================================
+# KNOWLEDGE ENGINE V4
+# ==========================================================
+
+COMPONENT_GROUPS = {
+
+    "engine": [
+        "mesin",
+        "klep",
+        "camshaft",
+        "kruk",
+        "noken",
+        "bearing",
+        "connecting rod",
+        "piston",
+        "ring piston",
+        "head"
+    ],
+
+    "cvt": [
+        "roller",
+        "v belt",
+        "vbelt",
+        "kampas ganda",
+        "clutch",
+        "torque driver",
+        "bearing cvt"
+    ],
+
+    "transmission": [
+        "transmisi",
+        "gear",
+        "kopling",
+        "synchromesh",
+        "input shaft",
+        "output shaft"
+    ],
+
+    "brake": [
+        "cakram",
+        "kampas rem",
+        "kaliper",
+        "master rem",
+        "selang rem"
+    ],
+
+    "suspension": [
+        "shock",
+        "tie rod",
+        "rack steer",
+        "ball joint",
+        "bushing"
+    ],
+
+    "cooling": [
+        "radiator",
+        "waterpump",
+        "thermostat",
+        "coolant",
+        "kipas"
+    ]
+
+}
